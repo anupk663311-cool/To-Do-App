ac0904bf9327618e5ccbeed2c063b2302c2fd365
anupk663311-cool <anupk663311@gmail.com>
2026-02-21 05:41:51 +0530

    MyFirstCommit

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-let taskList = document.getElementById("taskList");
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+function login() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+    let msg = document.getElementById("msg");
 
-function renderTasks() {
-    taskList.innerHTML = "";
-    tasks.forEach((task, index) => {
-        let li = document.createElement("li");
-        li.innerHTML = `${task} <span onclick="deleteTask(${index})">❌</span>`;
-        taskList.appendChild(li);
-    });
-}
-
-function addTask() {
-    let input = document.getElementById("taskInput");
-    if (input.value === "") {
-        alert("Please enter a task");
-        return;
+    // demo credentials
+    if (user === "admin" && pass === "1234") {
+        msg.style.color = "green";
+        msg.innerText = "Login Successful ✅";
+    } else {
+        msg.style.color = "red";
+        msg.innerText = "Invalid Username or Password ❌";
     }
-    tasks.push(input.value);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    input.value = "";
-    renderTasks();
 }
-
-function deleteTask(index) {
-    tasks.splice(index, 1);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    renderTasks();
-}
-
-renderTasks();
